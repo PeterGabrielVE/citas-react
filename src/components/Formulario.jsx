bd4f3ff54dc20937c499d5feac12cf0a191cc0b8
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-04-27 09:06:59 -0400

    input

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -5,10 +5,16 @@ function Formulario(){
     <div className="md:w-1/2 lg:w-2/5">
        <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
       
-      <p className="text-lg mt-5 text-center">Añade Pacientes y {''}
+      <p className="text-lg mt-5 text-center mb-10">Añade Pacientes y {''}
       <span className="text-indigo-600 font-bold">Administralos</span></p>
 
-      <form></form>
+      <form className="bg-white shadow-md rounded-lg py-10 px-5">
+        <div>
+          <label htmlFor="mascota" className="block">Nombre Mascota</label>
+          <input id="mascota" type="text" placeholder="Nombre de la Mascota" 
+          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+        </div>
+      </form>
     </div>
   )   
 }
