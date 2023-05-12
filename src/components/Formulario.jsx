b11ac9a1331c582e6bda29a8cf334089c7cc1f69
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-05-11 20:40:59 -0400

    eventos

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,6 +2,14 @@ import { useState, useEffect } from 'react';
 
 function Formulario(){
 
+  const [nombre, setNombre] = useState('');
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+
+    console.log('Enviando Formularioooo');
+  }
+
   return (
     <div className="md:w-1/2 lg:w-2/5">
        <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
@@ -9,11 +17,15 @@ function Formulario(){
       <p className="text-lg mt-5 text-center mb-10">Añade Pacientes y {''}
       <span className="text-indigo-600 font-bold">Administralos</span></p>
 
-      <form className="bg-white shadow-md rounded-lg py-10 px-5 text-left mb-10">
+      <form onSubmit={ handleSubmit }
+      className="bg-white shadow-md rounded-lg py-10 px-5 text-left mb-10">
         <div className="mb-5">
           <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
           <input id="mascota" type="text" placeholder="Nombre de la Mascota" 
-          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+          className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          value={nombre}
+          onChange={ (e) => setNombre(e.target.value) }
+          />
         </div>
 
         <div className="mb-5">
