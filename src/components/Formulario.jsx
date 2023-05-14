f84704afaf4c453fa2bd72b8421e5b63d73ee1f4
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-05-14 11:49:21 -0400

    mostrar mensaje de error

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -8,15 +8,18 @@ function Formulario(){
   const [fecha, setFecha] = useState('');
   const [sintomas, setSintomas] = useState('');
 
+  const [error, setError] = useState('');
+
   const handleSubmit = (e) =>{
     e.preventDefault();
 
     if([ nombre, propietario, email, fecha, sintomas ].includes('')){
       console.log('Hay al menos un campo vacio');
-    }else{
-      console.log('Todos llenos');
+      setError(true)
+      return;
     }
    
+    setError(false)
   }
 
   return (
@@ -28,6 +31,9 @@ function Formulario(){
 
       <form onSubmit={ handleSubmit }
       className="bg-white shadow-md rounded-lg py-10 px-5 text-left mb-10">
+        { error && (<div className='bg-red-800 text-white p-3 uppercase font-bold mb-3 rounded-md'>
+          <p>Todos los campos son obligatorios</p>
+          </div>) }
         <div className="mb-5">
           <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
           <input id="mascota" type="text" placeholder="Nombre de la Mascota" 
