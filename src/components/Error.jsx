54b4b889b995bb1d0736b446607736ee34ce09be
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-05-16 11:11:18 -0400

    props children

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Error = ({ mensaje }) => {
+const Error = ({ children }) => {
   return (
     <div className='bg-red-800 text-white p-3 uppercase font-bold mb-3 rounded-md'>
-          <p>{ mensaje }</p>
+          <p>{ children }</p>
     </div>
   )
 }
