7638c4980ecfbe1aaec3a785d36516174b174d52
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-05-15 21:56:13 -0400

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Header from './components/Header';
 import Formulario  from './components/Formulario';
 import ListadoPacientes  from './components/ListadoPacientes';
@@ -6,9 +7,15 @@ import './App.css'
 
 function App() {
  
+  const [pacientes, setPacientes] = useState([]);
+
+  const toma1valor = (valor)  =>{
+    console.log(valor)
+  }
+
   return (
     <div className="container mx-auto mt-20">
-      <Header />
+      <Header toma1valor={toma1valor} />
       <div className="mt-12 md:flex">
         <Formulario />
         <ListadoPacientes />
